2e71c782331e78ad096fc42cc764d97c658ddd8c
wrumble <rumblewayne@gmail.com>
2016-12-12 13:47:27 +0000

    adds working hitlist with coredata

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -7,11 +7,91 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var people = [NSManagedObject]()
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    @IBAction func addName(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: { (action:UIAlertAction) -> Void in
+                                        
+            let textField = alert.textFields!.first
+            self.saveName(textField!.text!)
+            self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (action: UIAlertAction) -> Void in
+            
+        }
+        
+        alert.addTextFieldWithConfigurationHandler { (textField: UITextField) -> Void in
+            
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func saveName(name: String) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text = person.valueForKey("name") as? String
+        
+        return cell!
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        
+        do {
+            let results =
+                try managedContext.executeFetchRequest(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
